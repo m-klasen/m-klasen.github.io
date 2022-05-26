ad46ad39653bea0489300e6ee4eebf1aba4a24dc
mklasen <morris.klasen@gmail.com>
2022-05-26 16:00:37 +0200

    Add files via upload

---
+++ webpage/homepage_files/a_data/app.js
@@ -0,0 +1,35 @@
+var mgharbi_app = angular.module('mgharbi', [
+    'ngRoute',
+    'mgharbi.controllers',
+    'mgharbi.services'
+]);
+
+mgharbi_app.config(
+    function($routeProvider, $locationProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'partials/home.html',
+                controller: 'MainController'
+            }).
+            when('/projects/gaussian', {
+                templateUrl: 'partials/project_gaussian.html',
+                controller: 'MainController'
+            }).
+            when('/bio', {
+                templateUrl: 'partials/bio.html',
+                controller: 'MainController'
+            }).
+            otherwise({
+                redirectTo: ''
+            });
+
+        $locationProvider.html5Mode({
+            enabled: true
+        });
+});
+
+$(document).ready(function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+});
